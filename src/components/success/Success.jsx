4a52a6d284e98ad95a7ed6434adce247500847ab
Ana Sofia Ricardo Aguilar <anasofiaricardoaguilar@gmail.com>
2023-06-21 18:15:40 -0500

    21/06/2023 6:16

--- src/components/success/Success.jsx
+++ src/components/success/Success.jsx
@@ -4,7 +4,9 @@ import classes from './success.module.css'
 const Success = () => {
   return (
     <div className={classes.successMessage}>
-      You have successfully made a payment!
+      ¡Ha realizado un pago con éxito!
+      <img src='https://res.cloudinary.com/drnmx4xgd/image/upload/v1687388184/WhatsApp_Image_2023-06-21_at_12.39.01_PM-removebg-preview_n4ha4p.png'/>
+      <a href='/Home'>Volver al Inicio</a>
     </div>
   )
 }
